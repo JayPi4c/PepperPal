deb0e1d70e65d52476dbd65cda1b58b4ede24965
Jonas Pohl <jonas.jp4c@gmail.com>
2023-04-11 17:04:46 +0200

    Finishing changes

--- src/main/java/com/jaypi4c/chilirestapi/config/DatabaseConfig.java
+++ src/main/java/com/jaypi4c/chilirestapi/config/DatabaseConfig.java
@@ -1,27 +1,21 @@
 package com.jaypi4c.chilirestapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-
+import java.util.Objects;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Autowired
-    Environment env;
-
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(Environment env) {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("driverClassName"));
-        dataSource.setUrl(env.getProperty("url"));
-        dataSource.setUsername(env.getProperty("user"));
-        dataSource.setPassword(env.getProperty("password"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("sqlite.driver-class-name")));
+        dataSource.setUrl(env.getProperty("sqlite.url"));
         return dataSource;
     }
 }
