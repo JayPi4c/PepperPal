e57844463d66e2930fcd0e0af974c6039fd57023
Jonas Pohl <git@jaypi4c.de>
2026-06-30 20:56:59 +0200

    Update to new namespace

---
+++ api/src/main/java/de/jaypi4c/pepperpal/api/exception/SoilDataNotFoundException.java
@@ -0,0 +1,10 @@
+package de.jaypi4c.pepperpal.api.exception;
+
+import java.util.UUID;
+
+public class SoilDataNotFoundException extends RuntimeException {
+
+    public SoilDataNotFoundException(UUID id) {
+        super("Could not find employee " + id);
+    }
+}
